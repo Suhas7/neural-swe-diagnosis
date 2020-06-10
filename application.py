771c1d56847a48e52a3dcc51d28d0863b9e24d50
Suhas <suhasraja1@gmail.com>
2020-06-10 13:29:57 -0500

    made an xgb that uses parent nodes

--- application.py
+++ application.py
@@ -2,8 +2,11 @@ import timesynth as ts
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error
 
 Features=5
+tt_split=.8
 
 class Application:
 	def generateData(self):
@@ -21,13 +24,23 @@ class Application:
 			a = np.add(a,self.features[:,i]*rd.gauss(1,.2))
 		self.target=np.asarray([a]).T
 		self.data=np.concatenate((self.features.T,self.target.T)).T
-
 	def __init__(self,ID):
 		self.ID=ID
 		self.parents = []
 		self.children = []
 		self.generateData()
-	
+		self.model=XGBRegressor()
 	def addParent(self,other):
 		self.parents.append(other)
-		self.target+=other.target*abs(rd.gauss(0,.2))
+		self.target=np.add(self.target,other.target*abs(rd.gauss(0,.2)))
+	def fitAndTest(self,includeParents=False):
+		if not includeParents:
+			self.model.fit(self.features[:int(500*tt_split),:],self.target[:int(500*tt_split),:])
+			yPred = self.model.predict(self.features[int(500*tt_split):,:])
+		else:
+			X=self.features
+			for par in self.parents:
+				X=np.concatenate(X.T,par.target.T).T
+			self.model.fit(X[:int(500*tt_split),:],self.target[:int(500*tt_split),:])
+			yPred = self.model.predict(X[int(500*tt_split):,:])
+		print("ID: {} Score: {}".format(self.ID,mean_squared_error(self.target[int(500*tt_split):,:],yPred)))
